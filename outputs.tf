d28a89218d70582ae41e5647cecfaf9ba12abc27
AkashSivakumar <akash@pearlthoughts>
2022-07-09 14:34:04 +0530

    Added outputs for custom domain

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "invoke_url" {
   value = aws_api_gateway_stage.example.invoke_url
 }
+
+output "api_id" {
+  value = aws_api_gateway_account.demo.id
+}
+
+output "stage_name1" {
+  value = aws_api_gateway_stage.example.stage_name
+}
